b9b4b020921929f45d57600b2e096172b43e6634
valeriy.tereshenko <tereshenko.valeriy.v@gmail.com>
2015-07-15 17:36:34 +0300

    init

---
+++ app/app.js
@@ -0,0 +1,17 @@
+/**
+ * Created on 15.07.2015.
+ */
+var app = angular.module('scopedListsApp', ['ngRoute', 'customSections']);
+
+app.config(function($routeProvider){
+    $routeProvider
+        .when('/demo/screenWithLists', {
+            templateUrl: 'demo/screenWithLists.html'
+        })
+        .when('/demo', {
+            templateUrl: 'demo/main.html'
+        })
+        .otherwise({
+            templateUrl: 'main/view.html'
+        });
+});
---
+++ app/directives/sectionsDirective.js
@@ -0,0 +1,28 @@
+﻿angular.module('customSections', [])
+    .directive("sectionPlaceholder", function($compile, $route, $rootScope) {
+        return {
+            restrict: 'AC',
+            link: function(scope, element, attr) {
+                // Store the placeholder element for later use
+                $rootScope["placeholder_" + attr.sectionPlaceholder] = element[0];
+
+                // Clear the placeholder when navigating
+                $rootScope.$on('$routeChangeSuccess', function(e, a, b) {
+                    element.html('');
+                });
+            }
+        };
+    })
+    .directive("sectionItem", function($compile, $route, $rootScope) {
+        return {
+            restrict: 'AC',
+            link: function(scope, element, attr) {
+                // Locate the placeholder element
+                var targetElement = angular.element($rootScope["placeholder_" + attr.sectionItem]);
+
+                // Compile the template and bind it to the current scope, and inject it into the placeholder
+                targetElement.html(element.html());
+                $compile(targetElement)(scope);
+            }
+        };
+    });
